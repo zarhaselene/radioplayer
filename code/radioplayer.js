923e58e7bacbb0543265b31920ac00d4ead14466
Zarha <zarhaselene@gmail.com>
2024-11-19 09:11:04 +0100

    Steg 1. Gör en fetch

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -1,5 +1,15 @@
 // Steg 1. Gör en fetch till 'https://api.sr.se/api/v2/channels/?format=json'
 
+const channelsEl = document.getElementById("channels");
+
+async function getRadio() {
+  const response = await fetch(
+    "https://api.sr.se/api/v2/channels/?format=json"
+  );
+  const data = await response.json();
+}
+
+getRadio();
 // Steg 2. loopa med tex forEach över data.channels - ta ut data och visa på html-sidan.
 
 // Steg 3. ta ut liveaudio.url från varje kanal och lägg i en audio tagg.
